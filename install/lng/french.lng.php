7b4345c2fc61b1719ed1fd567fdc4a6d17714db3
Florian Lippert <flo@syscp.org>
2004-10-08 19:23:09 +0000

    Franzoesisches Languagepack hinzugefuegt. Vielen Dank an Tim Zielosko!
    git-svn-id: file:///var/svn/trunk/syscp@48 45fdb5c4-e40b-0410-b369-9aab4fe9a275

---
+++ install/lng/french.lng.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * filename: $Source$
+ * begin: Friday, Oct 08, 2004
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * @author Tim Zielosko <mail@zielosko.net>
+ * @copyright (C) 2004 Tim Zielosko
+ * @package Language
+ * @version $Id$
+ */
+
+/**
+ * Begin
+ */
+$lng['install']['welcome'] = 'Bienvenue à l´installation de SysCP';
+$lng['install']['welcometext'] = 'Merci beacoup d´avoir choisi SysCP. Pour installer SysCP remplissez les cases ci-dessous avec les dates demandées. <b>Attention:</b> Une banque de données déjà existante qui a le même nom que vous choisissez ci-dessous va être effacée!';
+$lng['install']['database'] = 'Banque de données';
+$lng['install']['mysql_hostname'] = 'Hostname MySQL';
+$lng['install']['mysql_database'] = 'Banque de données MySQL';
+$lng['install']['mysql_unpriv_user'] = 'Utilisateur pour l´accès inprivilégié à MySQL';
+$lng['install']['mysql_unpriv_pass'] = 'Mot de passe pour l´accès inprivilégié à MySQL';
+$lng['install']['mysql_root_user'] = 'Utilisateur pour l´accès root à MySQL';
+$lng['install']['mysql_root_pass'] = 'Mot de passe pour l´accès root à MySQL';
+$lng['install']['admin_account'] = 'Accès administrative';
+$lng['install']['admin_user'] = 'Login de l´administrateur';
+$lng['install']['admin_pass'] = 'Mot de passe de l´administrateur';
+$lng['install']['admin_pass_confirm'] = 'Mot de passe de l´administrateur (Confirmation)';
+$lng['install']['serversettings'] = 'Configuration du server';
+$lng['install']['servername'] = 'Nom du server (FQDN)';
+$lng['install']['serverip'] = 'Adresse IP du server';
+$lng['install']['next'] = 'Continuer';
+
+/**
+ * Progress
+ */
+$lng['install']['testing_mysql'] = 'Verifiant le login root de MySQL...';
+$lng['install']['erasing_old_db'] = 'Effacant la vielle banque de données...';
+$lng['install']['create_mysqluser_and_db'] = 'Créant banque de données et utilisateur...';
+$lng['install']['testing_new_db'] = 'Verifiant banque de données et utilisateur...';
+$lng['install']['importing_data'] = 'Important les données dans la banque de données...';
+$lng['install']['changing_data'] = 'Conformant les données importés...';
+$lng['install']['adding_admin_user'] = 'Appliquant l´administrateur...';
+$lng['install']['creating_configfile'] = 'Créant le fichier de configuration...';
+$lng['install']['creating_configfile_succ'] = 'OK, userdata.inc.php était sauvegardé à lib/.';
+$lng['install']['creating_configfile_temp'] = 'Fichier était sauvegardé à /tmp/userdata.inc.php, s´il-vous plait le déplacer à lib/.';
+$lng['install']['creating_configfile_failed'] = 'Erreur en créant lib/userdata.inc.php, s´il-vous plait créez le avec le content ci-dessous:';
+$lng['install']['syscp_succ_installed'] = 'SysCP était installé avec succès.';
+$lng['install']['click_here_to_login'] = 'Continuer à l´écran login.';
+
+?>
---
+++ lng/french.lng.php
@@ -0,0 +1,268 @@
+<?php
+/**
+ * filename: $Source$
+ * begin: Friday, Oct 08, 2004
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * @author Tim Zielosko <tim@zielosko.de>
+ * @copyright (C) 2004 Tim Zielosko
+ * @package Language
+ * @version $Id$
+ */
+
+
+/**
+ * Global
+ */
+$lng['panel']['edit'] = 'Modifier';
+$lng['panel']['delete'] = 'Effacer';
+$lng['panel']['create'] = 'Appliquer';
+$lng['panel']['save'] = 'Sauvegarder';
+$lng['panel']['yes'] = 'Oui';
+$lng['panel']['no'] = 'Non';
+$lng['panel']['emptyfornochanges'] = 'Veuillez laisser vide pour aucun changement';
+$lng['panel']['emptyfordefault'] = 'Veuillez laisser vide pour l´option standard';
+$lng['panel']['path'] = 'Chemin';
+
+/**
+ * Login
+ */
+$lng['login']['username'] = 'Identifiant';
+$lng['login']['password'] = 'Mot de passe';
+$lng['login']['language'] = 'Langue';
+$lng['login']['login'] = 'S´identifier';
+$lng['login']['logout'] = 'Se deconnecter';
+
+/**
+ * Customer
+ */
+$lng['customer']['login'] = 'Identifiant';
+$lng['customer']['documentroot'] = 'Chemin';
+$lng['customer']['name'] = 'Nom';
+$lng['customer']['surname'] = 'Prénom';
+$lng['customer']['company'] = 'Entreprise';
+$lng['customer']['street'] = 'Rue';
+$lng['customer']['zipcode'] = 'Code postal';
+$lng['customer']['city'] = 'Ville';
+$lng['customer']['phone'] = 'Téléphone';
+$lng['customer']['fax'] = 'Fax';
+$lng['customer']['email'] = 'e-mail';
+$lng['customer']['customernumber'] = 'Numero du client';
+$lng['customer']['diskspace'] = 'Webspace (MB)';
+$lng['customer']['traffic'] = 'Traffic (GB)';
+$lng['customer']['mysqls'] = 'Banque(s) de données MySQL';
+$lng['customer']['emails'] = 'Adresse(s) e-mail';
+$lng['customer']['forwarders'] = 'Retransmissions e-mail';
+$lng['customer']['ftps'] = 'Accès FTP';
+$lng['customer']['subdomains'] = 'Sub-Domain(s)';
+$lng['customer']['domains'] = 'Domain(s)';
+$lng['customer']['unlimited'] = 'illimité';
+
+/**
+ * Customermenue
+ */
+$lng['menue']['main']['main'] = 'General';
+$lng['menue']['main']['changepassword'] = 'Changer le mot de passe';
+$lng['menue']['email']['email'] = 'e-mail';
+$lng['menue']['email']['pop'] = 'Accès POP3';
+$lng['menue']['email']['forwarders'] = 'Retransmissions';
+$lng['menue']['email']['webmail'] = 'Webmail';
+$lng['menue']['mysql']['mysql'] = 'MySQL';
+$lng['menue']['mysql']['databases'] = 'Banques de données';
+$lng['menue']['mysql']['phpmyadmin'] = 'phpMyAdmin';
+$lng['menue']['domains']['domains'] = 'Domains';
+$lng['menue']['domains']['settings'] = 'Réglages';
+$lng['menue']['ftp']['ftp'] = 'FTP';
+$lng['menue']['ftp']['accounts'] = 'Accès';
+$lng['menue']['ftp']['webftp'] = 'WebFTP';
+$lng['menue']['extras']['extras'] = 'Extras';
+$lng['menue']['extras']['directoryprotection'] = 'Protection des dossiers';
+$lng['menue']['extras']['pathoptions'] = 'Options du chemin';
+
+/**
+ * Index
+ */
+$lng['index']['customerdetails'] = 'Données du compte';
+$lng['index']['accountdetails'] = 'Données de l´accès';
+
+/**
+ * Change Password
+ */
+$lng['changepassword']['old_password'] = 'Vieux mot de passe';
+$lng['changepassword']['new_password'] = 'Nouveau mot de passe';
+$lng['changepassword']['new_password_confirm'] = 'Nouveau mot de passe (confirmer)';
+$lng['changepassword']['also_change_ftp'] = ' Changer aussi le mot de passe de l´accès FTP general';
+
+/**
+ * Domains
+ */
+$lng['domains']['description'] = 'Ici vous pouvez inscrire des Domains et changer ses chemins.<br />Il faut un peu de temps après chaque changement pour relire la configuration.';
+$lng['domains']['domainsettings'] = 'Configuration des Domains';
+$lng['domains']['domainname'] = 'Nom du Domain';
+$lng['domains']['subdomain_add'] = 'Appliquer un Subdomain';
+$lng['domains']['subdomain_edit'] = 'Changer un Subdomain';
+$lng['domains']['wildcarddomain'] = 'Domain Wildcard?';
+
+/**
+ * eMails
+ */
+$lng['emails']['description'] = 'Ici vous pouvez appliquer vos boites à e-mail.<br><br>Les données pour configurer votre logiciel e-mail sont celles-la: <br><br>Nom du server: <b><i>votre domain</i></b><br>Identifiant: <b><i>l´adresse e-mail</i></b><br>Mot de passe: <b><i>le mot de passe que vous avez choisi</i></b>';
+$lng['emails']['forwarders_add'] = 'Appliquer une retransmission';
+$lng['emails']['from'] = 'de';
+$lng['emails']['to'] = 'à';
+$lng['emails']['pop3_add'] = 'Appliquer un accès POP3';
+$lng['emails']['emailaddress'] = 'Accès / Adresse';
+
+/**
+ * FTP
+ */
+$lng['ftp']['description'] = 'Ici vous pouvez appliquer des accès FTP additionel.<br />Les changements sont tout de suite opérant et l´accès est disponible.';
+$lng['ftp']['account_add'] = 'Appliquer un accès';
+
+/**
+ * MySQL
+ */
+$lng['mysql']['description'] = 'Ici vous pouvez appliquer et effacer des banques de données MySQL.<br>Les changements sont tout de suite opérant et les banques sont disponibles.<br>Sur le menu on trouve un lien à phpMyAdmin, avec lequel vous pouvez modifier vos banques de données.<br><br>L´accès de PHP fonctionne comme ca: (Il faut modifier les valeurs en <i>italique</i> en mettant ce que c´est!)<br><br>$connection = mysql_connect("localhost", "<i>Votre identifiant</i>", "<i>Votre mot de passe</i>");<br>mysql_select_db("<i>Le nom de la banque</i>", $connection);';
+$lng['mysql']['databasename'] = 'Nom de la banque';
+$lng['mysql']['database_create'] = 'Appliquer une banque de données';
+
+/**
+ * Extras
+ */
+$lng['extras']['description'] = 'Ici vous pouvez appliquer des extras additionell, par example la protection des listes.<br />Il faut un peu de temps après chaque changement pour reliser la configuration.';
+$lng['extras']['directoryprotection_add'] = 'Appliquer une protection des dossiers';
+$lng['extras']['view_directory'] = 'Faire voir le fichier';
+$lng['extras']['pathoptions_add'] = 'Appliquer des options de chemin';
+$lng['extras']['directory_browsing'] = 'Faire voir le contenu des fichiers';
+$lng['extras']['pathoptions_edit'] = 'Modifier les options de chemin';
+
+/**
+ * Errors
+ */
+$lng['error']['error'] = 'Erreur';
+$lng['error']['directorymustexist'] = 'Le dossier que vous avez choisi n´existe pas. S´il vous plait appliquer le avec votre client FTP.';
+$lng['error']['domains_cantdeletemaindomain'] = 'Vous ne pouvez pas effacer une domain qui est utilisé pour des adresses e-mail. ';
+$lng['error']['firstdeleteallsubdomains'] = 'Il faut effacer toutes les subdomains avant d´appliquer un domain Wildcard.';
+$lng['error']['ftp_cantdeletemainaccount'] = 'Vous ne pouvez pas effacer votre accès principal.';
+$lng['error']['login'] = 'Identifiant / mot de passe invalide.';
+$lng['error']['login_blocked'] = 'Cet accès était bloqué à cause de trop des login fautes.<br />S´il vous-plait l´essayer encore dans '.$settings['login']['deactivatetime'].' secondes.';
+$lng['error']['notallreqfieldsorerrors'] = 'Vous n´avez pas rempli toutes les cases ou vous avez rempli des valeurs invalide.';
+$lng['error']['oldpasswordnotcorrect'] = 'Le vieux mot de passe n´est pas correct.';
+$lng['error']['youcantallocatemorethanyouhave'] = 'Vous ne pouvez pas distribuer plus des ressources qu´il reste.';
+$lng['error']['youcantdeletechangemainadmin'] = 'Pour des raisons de la sécurité c´est pas possible d´effacer ou modifier l´administrateur principal.';
+
+/**
+ * Questions
+ */
+$lng['question']['question'] = 'Question de sécurité';
+$lng['question']['admin_customer_reallydelete'] = 'Voulez-vous vraiment effacer ce compte? ATTENTION! Toutes les données vont être effacées! Après ceci fait il faut effacer les dossiers du system des fichiers manuellement.';
+$lng['question']['admin_domain_reallydelete'] = 'Voulez-vous vraiment effacer ce domain?';
+$lng['question']['admin_domain_reallydisablesecuritysetting'] = 'Voulez-vous vraiment désactiver ces modes importants (OpenBasedir et/où SafeMode) ?';
+$lng['question']['admin_admin_reallydelete'] = 'Voulez-vous vraiment effacer cet administrateur? Tout ses comptes vont être affecté au administrateur principal.';
+$lng['question']['domains_reallydelete'] = 'Voulez-vous vraiment effacer ce domain?';
+$lng['question']['email_reallydelete_forwarders'] = 'Voulez-vous vraiment effacer cette retransmission?';
+$lng['question']['email_reallydelete_pop'] = 'Voulez-vous vraiment effacer cette adresse e-mail?';
+$lng['question']['extras_reallydelete'] = 'Voulez-vous vraiment effacer cette protection du dossier?';
+$lng['question']['extras_reallydelete_pathoptions'] = 'Voulez-vous vraiment effacer les options de ce chemin?';
+$lng['question']['ftp_reallydelete'] = 'Voulez-vous vraiment effacer cet accès POP3?';
+$lng['question']['mysql_reallydelete'] = 'Voulez-vous vraiment effacer cette banque de donnés? ATTENTION: Toutes les données vont être effacées!';
+
+/**
+ * Mails
+ */
+$lng['mails']['pop_success']['mailbody'] = 'Bonjour,\n\nvotre accès POP3 $email\na été installé avec succès.\n\nC´est un e-mail géneré automatiquement, s´il vous plait ne repondez pas a ce message.\n\nVotre Webmaster';
+$lng['mails']['pop_success']['subject'] = 'Accès POP3 installé';
+$lng['mails']['createcustomer']['mailbody'] = 'Bonjour $surname $name,\n\nici vos informations d´accès:\n\nIdentifiant: $loginname\nMot de passe: $password\n\nNous vous remercions,\nVotre Webmaster';
+$lng['mails']['createcustomer']['subject'] = 'Informations de votre accès';
+
+/**
+ * Admin
+ */
+$lng['admin']['overview'] = 'Sommaire';
+$lng['admin']['ressourcedetails'] = 'Ressources utilisés';
+$lng['admin']['systemdetails'] = 'Details du system';
+$lng['admin']['syscpdetails'] = 'Details du SysCP';
+$lng['admin']['installedversion'] = 'Version installée';
+$lng['admin']['latestversion'] = 'La plus nouvelle version';
+$lng['admin']['lookfornewversion']['clickhere'] = 'Interroger par internet';
+$lng['admin']['lookfornewversion']['error'] = 'Erreur en triant';
+$lng['admin']['customer'] = 'Compte';
+$lng['admin']['customers'] = 'Comptes';
+$lng['admin']['customer_add'] = 'Appliquer un compte';
+$lng['admin']['customer_edit'] = 'Modifier un compte';
+$lng['admin']['domains'] = 'Domains';
+$lng['admin']['domain_add'] = 'Appliquer un Domain';
+$lng['admin']['domain_edit'] = 'Modifier le domain';
+$lng['admin']['admin'] = 'Administrateur';
+$lng['admin']['admins'] = 'Administrateurs';
+$lng['admin']['admin_add'] = 'Appliquer un administrateur';
+$lng['admin']['admin_edit'] = 'Modifier un administrateur';
+$lng['admin']['customers_see_all'] = 'Peut voir tous les comptes?';
+$lng['admin']['domains_see_all'] = 'Peut voir tous les Domains?';
+$lng['admin']['change_serversettings'] = 'Peut modifier la configuration du server?';
+$lng['admin']['serversettings'] = 'Configuration du server';
+$lng['admin']['stdsubdomain'] = 'Subdomain-type';
+$lng['admin']['stdsubdomain_add'] = 'Appliquer un subdomain-type';
+$lng['admin']['deactivated'] = 'Bloqué';
+$lng['admin']['deactivated_user'] = 'Bloquer utilisateur';
+$lng['admin']['sendpassword'] = 'Envoyer mot de passe';
+$lng['admin']['ownvhostsettings'] = 'Configuration spéciale du vHost';
+$lng['admin']['configfiles']['serverconfiguration'] = 'Configuration du server';
+$lng['admin']['configfiles']['files'] = '<b>Fichiers de configuration:</b> S´il vous-plait modifiez les fichiers correspondants<br />ou créez les avec les contenu ci-dessous.<br /><b>IMPORTANT:</b> Le mot de passe MySQL n´est pas donnés dans les dates ci-dessus<br />à cause des raisons de sécurité. S´il vous-plait substituez les &quot;MYSQL_PASSWORD&quot;<br />manuellement avec le mot de passe. En cas de l´avoir oublié, vous le trouvez dans<br />le fichier &quot;lib/userdata.inc.php&quot;.';
+$lng['admin']['configfiles']['commands'] = '<b>Commandes:</b> S´il vous-plait exécuter les commandes ci-dessous sur le shell.';
+$lng['admin']['configfiles']['restart'] = '<b>Rédemarrer:</b> S´il vous-plait exécuter les commandes ci-dessous pour<br />réinitialiser les fichiers de configuration.';
+
+/**
+ * Serversettings
+ */
+$lng['serversettings']['session_timeout']['title'] = 'Session Timeout';
+$lng['serversettings']['session_timeout']['description'] = 'Combien de temps faut-il etre inactif pour que votre session se ferme automatiquement (secondes)';
+$lng['serversettings']['catachallkeyword']['title'] = 'Catchall-Keyword';
+$lng['serversettings']['catachallkeyword']['description'] = 'Quelle est l´adresse e-mail qui capture tous e-mails?';
+$lng['serversettings']['accountprefix']['title'] = 'Préfix des comptes';
+$lng['serversettings']['accountprefix']['description'] = 'Quel préfix doivent avoir les comptes?';
+$lng['serversettings']['mysqlprefix']['title'] = 'Préfix SQL';
+$lng['serversettings']['mysqlprefix']['description'] = 'Quel préfix doivent avoir les banques de données?';
+$lng['serversettings']['ftpprefix']['title'] = 'Préfix FTP';
+$lng['serversettings']['ftpprefix']['description'] = 'Quel préfix doivent avoir les accès FTP?';
+$lng['serversettings']['documentroot_prefix']['title'] = 'Documentdirectory';
+$lng['serversettings']['documentroot_prefix']['description'] = 'Où doivent être tous les comptes';
+$lng['serversettings']['logfiles_directory']['title'] = 'Logfilesdirectory';
+$lng['serversettings']['logfiles_directory']['description'] = 'Où doivent être les archives d´accès?';
+$lng['serversettings']['ipaddress']['title'] = 'Adresse IP';
+$lng['serversettings']['ipaddress']['description'] = 'Quelle est l´adresse IP du server?';
+$lng['serversettings']['hostname']['title'] = 'Hostname';
+$lng['serversettings']['hostname']['description'] = 'Quel est le hostname du server?';
+$lng['serversettings']['apacheconf_directory']['title'] = 'Apache-Config-Directory';
+$lng['serversettings']['apacheconf_directory']['description'] = 'Où est sauvegardée la configuration de l´Apache?';
+$lng['serversettings']['apachereload_command']['title'] = 'Apache-Reload-Command';
+$lng['serversettings']['apachereload_command']['description'] = 'Comment est la commande pour redémarrer l´Apache?';
+$lng['serversettings']['bindconf_directory']['title'] = 'Bind-Config-Directory';
+$lng['serversettings']['bindconf_directory']['description'] = 'Où est sauvegardée la configuration du BIND?';
+$lng['serversettings']['bindreload_command']['title'] = 'Bind-Reload-Command';
+$lng['serversettings']['bindreload_command']['description'] = 'Comment est la commande pour redémarrer le BIND?';
+$lng['serversettings']['binddefaultzone']['title'] = 'Bind-Default-Zone';
+$lng['serversettings']['binddefaultzone']['description'] = 'Comment s´appelle la zone standard des tous les domaines?';
+$lng['serversettings']['vmail_uid']['title'] = 'Mails-Uid';
+$lng['serversettings']['vmail_uid']['description'] = 'Quel UID doivent avoir les e-mails?';
+$lng['serversettings']['vmail_gid']['title'] = 'Mails-Gid';
+$lng['serversettings']['vmail_gid']['description'] = 'Quel GID doivent avoir les e-mails?';
+$lng['serversettings']['vmail_homedir']['title'] = 'Mails-Homedir';
+$lng['serversettings']['vmail_homedir']['description'] = 'Où doivent être les e-mails?';
+$lng['serversettings']['adminmail']['title'] = 'Adresse de l´expéditeur';
+$lng['serversettings']['adminmail']['description'] = 'Quelle est l´adresse standard des e-mails qui sont envoyés de SysCP?';
+$lng['serversettings']['phpmyadmin_url']['title'] = 'URL phpMyAdmin';
+$lng['serversettings']['phpmyadmin_url']['description'] = 'À quelle adresse se trouve le phpMyAdmin?';
+$lng['serversettings']['webmail_url']['title'] = 'URL WebMail';
+$lng['serversettings']['webmail_url']['description'] = 'À quelle adresse se trouve le WebMail?';
+$lng['serversettings']['webftp_url']['title'] = 'URL WebFTP';
+$lng['serversettings']['webftp_url']['description'] = 'À quelle adresse se trouve le WebFTP?';
+
+?>
