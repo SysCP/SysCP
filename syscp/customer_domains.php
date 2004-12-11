848aa49ea9aefff8fc5249305fc481a73031a673
Michael Duergner <duergner@syscp.org>
2004-12-11 13:40:50 +0000

    Little problem with the IDN Domain Patch solved; domainname was not decoded during security question when removing a domain, an email account or a forward;
    git-svn-id: file:///var/svn/trunk@90 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- syscp/customer_domains.php
+++ syscp/customer_domains.php
@@ -73,7 +73,7 @@
 					header("Location: $filename?page=$page&s=$s");
 				}
 				else {
-					ask_yesno('domains_reallydelete', $filename, "id=$id;page=$page;action=$action", $result['domain']);
+					ask_yesno('domains_reallydelete', $filename, "id=$id;page=$page;action=$action", $idna_convert->decode($result['domain']));
 				}
 			}
 			else
