552e972b299dd2a7afb408abd35384662ad63f38
Florian Lippert <flo@syscp.org>
2005-05-06 14:17:40 +0000

    Schedule for release 1.2.9
    git-svn-id: file:///var/svn/trunk/syscp@178 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- install/updatesql_1.2.inc.php
+++ install/updatesql_1.2.inc.php
@@ -593,10 +593,10 @@
 		$db->query("UPDATE `".TABLE_PANEL_SETTINGS."` SET `value`='1.2.8' WHERE `settinggroup`='panel' AND `varname`='version'");
 		$settings['panel']['version'] = '1.2.8';
 	}
-	if($settings['panel']['version'] == '1.2.8')
+	if($settings['panel']['version'] == '1.2.8' || $settings['panel']['version'] == '1.2.8-cvs1')
 	{
-		$db->query("UPDATE `".TABLE_PANEL_SETTINGS."` SET `value`='1.2.8-cvs1' WHERE `settinggroup`='panel' AND `varname`='version'");
-		$settings['panel']['version'] = '1.2.8-cvs1';
+		$db->query("UPDATE `".TABLE_PANEL_SETTINGS."` SET `value`='1.2.9' WHERE `settinggroup`='panel' AND `varname`='version'");
+		$settings['panel']['version'] = '1.2.9';
 	}
 	
 ?>
