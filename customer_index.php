c2fceb225b8a50eb334f0f211c084cd58c60e80d
Florian Lippert <flo@syscp.org>
2007-12-30 22:47:48 +0000

    Fixed bug #445
    git-svn-id: file:///var/svn/trunk/syscp@1357 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- customer_index.php
+++ customer_index.php
@@ -35,6 +35,7 @@ if($page == 'overview')
 	$domains = '';
 	$result = $db->query("SELECT `domain` FROM `" . TABLE_PANEL_DOMAINS . "` WHERE `customerid`='" . (int)$userinfo['customerid'] . "' AND `parentdomainid`='0' AND `id` <> '" . (int)$userinfo['standardsubdomain'] . "' ");
 
+	$domainArray = array();
 	while($row = $db->fetch_array($result))
 	{
 		$domainArray[] = $idna_convert->decode($row['domain']);
