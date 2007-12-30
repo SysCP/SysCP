df94ae1a5cc0a0b6d0f9a588198e91392079f5a4
Florian Aders <EleRas@syscp.org>
2007-12-30 14:38:18 +0000

    * Fixed typos   * Added missing GPL-Header
    git-svn-id: file:///var/svn/trunk@1350 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- syscp/scripts/cron_traffic.inc.functions.php
+++ syscp/scripts/cron_traffic.inc.functions.php
@@ -1,6 +1,22 @@
 <?php
 
 /**
+ * This file is part of the SysCP project.
+ * Copyright (c) 2003-2007 the SysCP Team (see authors).
+ *
+ * For the full copyright and license information, please view the COPYING
+ * file that was distributed with this source code. You can also view the
+ * COPYING file online at http://files.syscp.org/misc/COPYING.txt
+ *
+ * @copyright  (c) the authors
+ * @author     Florian Lippert <flo@syscp.org>
+ * @author     Florian Aders <eleras@syscp.org>
+ * @license    GPLv2 http://files.syscp.org/misc/COPYING.txt
+ * @package    System
+ * @version    $Id$
+ */
+
+/**
  * Function which make webalizer statistics and returns used traffic since last run
  *
  * @param string Name of logfile
