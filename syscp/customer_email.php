88f8f4485f851aebd9c4cbfb3d0148ec022621e1
Michael Kaufmann <d00p@syscp.org>
2008-05-10 19:50:38 +0000

    using phpmailer now to send mails
    git-svn-id: file:///var/svn/trunk@1905 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- syscp/customer_email.php
+++ syscp/customer_email.php
@@ -418,7 +418,16 @@ elseif($page == 'accounts')
 						$mail_subject = html_entity_decode(replace_variables((($result['value'] != '') ? $result['value'] : $lng['mails']['pop_success']['subject']), $replace_arr));
 						$result = $db->query_first('SELECT `value` FROM `' . TABLE_PANEL_TEMPLATES . '` WHERE `adminid`=\'' . (int)$userinfo['adminid'] . '\' AND `language`=\'' . $db->escape($userinfo['def_language']) . '\' AND `templategroup`=\'mails\' AND `varname`=\'pop_success_mailbody\'');
 						$mail_body = html_entity_decode(replace_variables((($result['value'] != '') ? $result['value'] : $lng['mails']['pop_success']['mailbody']), $replace_arr));
-						mail($email_full, $mail_subject, $mail_body, 'From: ' . buildValidMailFrom($admin['name'], $admin['email']));
+						$mail->From = $admin['email'];
+						$mail->FromName = $admin['name'];
+						$mail->Subject = $mail_subject;
+						$mail->Body = $mail_body;
+						$mail->AddAddress($email_full, $username);
+						if(!$mail->Send())
+						{
+							standard_error(array('errorsendingmail', $email_full));
+						}
+						$mail->ClearAddresses();
 
 						if(validateEmail($alternative_email)
 						   && $settings['panel']['sendalternativemail'] == 1)
@@ -427,7 +436,16 @@ elseif($page == 'accounts')
 							$mail_subject = replace_variables((($result['value'] != '') ? $result['value'] : $lng['mails']['pop_success_alternative']['subject']), $replace_arr);
 							$result = $db->query_first('SELECT `value` FROM `' . TABLE_PANEL_TEMPLATES . '` WHERE `adminid`=\'' . (int)$userinfo['adminid'] . '\' AND `language`=\'' . $db->escape($userinfo['def_language']) . '\' AND `templategroup`=\'mails\' AND `varname`=\'pop_success_alternative_mailbody\'');
 							$mail_body = replace_variables((($result['value'] != '') ? $result['value'] : $lng['mails']['pop_success_alternative']['mailbody']), $replace_arr);
-							mail($alternative_email, $mail_subject, $mail_body, 'From: ' . buildValidMailFrom($admin['name'], $admin['email']));
+							$mail->From = $admin['email'];
+							$mail->FromName = $admin['name'];
+							$mail->Subject = $mail_subject;
+							$mail->Body = $mail_body;
+							$mail->AddAddress($alternative_email, $username);
+							if(!$mail->Send())
+							{
+								standard_error(array('errorsendingmail', $alternative_email));
+							}
+							$mail->ClearAddresses();
 						}
 
 						redirectTo($filename, Array(
