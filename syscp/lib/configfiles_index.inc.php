38d6efbf1fefbdf6b86449517d39c547cb093583
Michael Kaufmann <d00p@syscp.org>
2008-05-14 06:29:58 +0000

    added configuration templates for gentoo linux and disable ident and reverse DNS lookup in proftpd, fixes #604
    git-svn-id: file:///var/svn/trunk@1939 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- syscp/lib/configfiles_index.inc.php
+++ syscp/lib/configfiles_index.inc.php
@@ -312,6 +312,216 @@ $configfiles = Array(
 			)
 		)
 	),
+	'gentoo' => Array(
+		'label' => 'Gentoo',
+		'services' => Array(
+			'http' => Array(
+				'label' => $lng['admin']['configfiles']['http'],
+				'daemons' => Array(
+					'apache' => Array(
+						'label' => 'Apache2 Webserver',
+						'commands' => Array(
+							'touch ' . $settings['system']['apacheconf_vhost'],
+							'chown root:0 '.$settings['system']['apacheconf_vhost'],
+							'chmod 0600 '.$settings['system']['apacheconf_vhost'],
+							'touch ' . $settings['system']['apacheconf_diroptions'],
+							'chown root:0 '.$settings['system']['apacheconf_diroptions'],
+							'chmod 0600 '.$settings['system']['apacheconf_diroptions'],
+							'echo -e "\\nInclude ' . $settings['system']['apacheconf_vhost'] . '" >> ' . dirname($settings['system']['apacheconf_vhost']) . 'httpd.conf',
+							'mkdir -p ' . $settings['system']['documentroot_prefix'],
+							'mkdir -p ' . $settings['system']['logfiles_directory']						
+							),
+						'restart' => Array(
+							'rc-update add apache2 default',
+							'/etc/init.d/apache2 restart'
+						)
+					)
+				)
+			),
+			'dns' => Array(
+				'label' => $lng['admin']['configfiles']['dns'],
+				'daemons' => Array(
+					'bind' => Array(
+						'label' => 'Bind9 Nameserver',
+						'files' => Array
+						(
+							'etc_bind_default.zone' => '/etc/bind/default.zone'
+						),
+						'commands' => Array
+						(
+							'echo "include \"'.$settings['system']['bindconf_directory'].'syscp_bind.conf\";" >> /etc/bind/named.conf',
+							'touch '.$settings['system']['bindconf_directory'].'syscp_bind.conf',
+							'chown root:0 '.$settings['system']['bindconf_directory'].'syscp_bind.conf',
+							'chmod 0600 '.$settings['system']['bindconf_directory'].'syscp_bind.conf'
+						),
+						'restart' => Array
+						(
+							'rc-update add named default',
+							'/etc/init.d/named restart'
+						)
+					),
+				)
+			),
+			'mail' => Array(
+				'label' => $lng['admin']['configfiles']['mail'],
+				'daemons' => Array(
+					'courier' => Array(
+						'label' => 'Courier-IMAP (POP3/IMAP)',
+						'files' => Array(
+							'etc_courier_authlib_authdaemonrc' => '/etc/courier/authlib/authdaemonrc',
+							'etc_courier_authlib_authmysqlrc' => '/etc/courier/authlib/authmysqlrc',
+							'etc_courier-imap_pop3d' => '/etc/courier-imap/pop3d',
+							'etc_courier-imap_imapd' => '/etc/courier-imap/imapd',
+							'etc_courier-imap_pop3d-ssl' => '/etc/courier-imap/pop3d-ssl',
+							'etc_courier-imap_imapd-ssl' => '/etc/courier-imap/imapd-ssl'
+						),
+						'commands' => Array
+						(
+							'rm /etc/courier/authlib/authdaemonrc',
+							'rm /etc/courier/authlib/authmysqlrc',
+							'rm /etc/courier-imap/pop3d',
+							'rm /etc/courier-imap/imapd',
+							'rm /etc/courier-imap/pop3d-ssl',
+							'rm /etc/courier-imap/imapd-ssl',
+							'touch /etc/courier/authlib/authdaemonrc',
+							'touch /etc/courier/authlib/authmysqlrc',
+							'touch /etc/courier-imap/pop3d',
+							'touch /etc/courier-imap/imapd',
+							'touch /etc/courier-imap/pop3d-ssl',
+							'touch /etc/courier-imap/imapd-ssl',
+							'chown root:0 /etc/courier/authlib/authdaemonrc',
+							'chown root:0 /etc/courier/authlib/authmysqlrc',
+							'chown root:0 /etc/courier-imap/pop3d',
+							'chown root:0 /etc/courier-imap/imapd',
+							'chown root:0 /etc/courier-imap/pop3d-ssl',
+							'chown root:0 /etc/courier-imap/imapd-ssl',
+							'chmod 0600 /etc/courier/authlib/authdaemonrc',
+							'chmod 0600 /etc/courier/authlib/authmysqlrc',
+							'chmod 0600 /etc/courier-imap/pop3d',
+							'chmod 0600 /etc/courier-imap/imapd',
+							'chmod 0600 /etc/courier-imap/pop3d-ssl',
+							'chmod 0600 /etc/courier-imap/imapd-ssl'
+						),
+						'restart' => Array(
+							'rc-update add courier-authlib default',
+							'rc-update add courier-pop3d default',
+							'rc-update add courier-imapd default',
+							'/etc/init.d/courier-authlib restart',
+							'/etc/init.d/courier-pop3d restart',
+							'/etc/init.d/courier-imapd restart'
+						)
+					),
+				)
+			),
+			'smtp' => Array(
+				'label' => $lng['admin']['configfiles']['smtp'],
+				'daemons' => Array(
+					'postfix' => Array(
+						'label' => 'Postfix',
+						'files' => Array(
+							'etc_postfix_main.cf' => '/etc/postfix/main.cf',
+							'etc_postfix_mysql-virtual_alias_maps.cf' => '/etc/postfix/mysql-virtual_alias_maps.cf',
+							'etc_postfix_mysql-virtual_mailbox_domains.cf' => '/etc/postfix/mysql-virtual_mailbox_domains.cf',
+							'etc_postfix_mysql-virtual_mailbox_maps.cf' => '/etc/postfix/mysql-virtual_mailbox_maps.cf',
+							'etc_sasl2_smtpd.conf' => '/etc/sasl2/smtpd.conf'
+						),
+						'commands' => Array(
+							'mkdir -p '.$settings['system']['vmail_homedir'],
+							'chown -R vmail:vmail '.$settings['system']['vmail_homedir'],
+							'chmod 0750 '.$settings['system']['vmail_homedir'],
+							'rm /etc/postfix/main.cf',
+							'touch /etc/postfix/main.cf',
+							'touch /etc/postfix/master.cf',
+							'touch /etc/postfix/mysql-virtual_alias_maps.cf',
+							'touch /etc/postfix/mysql-virtual_mailbox_domains.cf',
+							'touch /etc/postfix/mysql-virtual_mailbox_maps.cf',
+							'touch /etc/sasl2/smtpd.conf',
+							'chown root:0 /etc/postfix/main.cf',
+							'chown root:0 /etc/postfix/master.cf',
+							'chown root:postfix /etc/postfix/mysql-virtual_alias_maps.cf',
+							'chown root:postfix /etc/postfix/mysql-virtual_mailbox_domains.cf',
+							'chown root:postfix /etc/postfix/mysql-virtual_mailbox_maps.cf',
+							'chown root:0 /etc/sasl2/smtpd.conf',
+							'chmod 0600 /etc/postfix/main.cf',
+							'chmod 0600 /etc/postfix/master.cf',
+							'chmod 0640 /etc/postfix/mysql-virtual_alias_maps.cf',
+							'chmod 0640 /etc/postfix/mysql-virtual_mailbox_domains.cf',
+							'chmod 0640 /etc/postfix/mysql-virtual_mailbox_maps.cf',
+							'chmod 0600 /etc/sasl2/smtpd.conf'
+						),
+						'restart' => Array(
+							'rc-update add postfix default',
+							'/etc/init.d/postfix restart'
+						)
+					),
+				)
+			),
+			'ftp' => Array(
+				'label' => $lng['admin']['configfiles']['ftp'],
+				'daemons' => Array(
+					'proftpd' => Array(
+						'label' => 'ProFTPd',
+						'files' => Array(
+							'etc_proftpd_proftpd.conf' => '/etc/proftpd/proftpd.conf'
+						),
+						'commands' => Array
+						(
+							'touch /etc/proftpd/proftpd.conf',
+							'chown root:0 /etc/proftpd/proftpd.conf',
+							'chmod 0600 /etc/proftpd/proftpd.conf'
+						),
+						'restart' => Array(
+							'rc-update add proftpd default',
+							'/etc/init.d/proftpd restart'
+						)
+					),
+				)
+			),
+			'etc' => Array(
+				'label' => $lng['admin']['configfiles']['etc'],
+				'daemons' => Array(
+					'cron' => Array(
+						'label' => 'Crond (cronscript)',
+						'files' => Array(
+							'etc_php_syscp-cronjob_php.ini' => '/etc/php/syscp-cronjob/php.ini',
+							'etc_cron.d_syscp' => '/etc/cron.d/syscp'
+						),
+						'commands' => Array
+						(
+							'touch /etc/cron.d/syscp',
+							'chown root:0 /etc/cron.d/syscp',
+							'chmod 0640 /etc/cron.d/syscp',
+							'mkdir -p /etc/php/syscp-cronjob',
+							'touch /etc/php/syscp-cronjob/php.ini',
+							'chown -R root:0 /etc/php/syscp-cronjob',
+							'chmod 0750 /etc/php/syscp-cronjob',
+							'chmod 0640 /etc/php/syscp-cronjob/php.ini'
+						),
+						'restart' => Array(
+							'rc-update add vixie-cron default',
+							'/etc/init.d/vixie-cron restart'
+						)
+					),
+					'libnss' => Array(
+						'label' => 'libnss (system login with mysql)',
+						'files' => Array(
+							'etc_libnss-mysql.cfg' => '/etc/libnss-mysql.cfg',
+							'etc_libnss-mysql-root.cfg' => '/etc/libnss-mysql-root.cfg'
+						),
+						'commands' => Array
+						(
+							'# if not installed:',
+							'emerge -av libnss-mysql'
+						),
+						'restart' => Array(
+							'rc-update add nscd default',
+							'/etc/init.d/nscd restart'
+						)
+					)
+				)
+			)
+		)
+	),
 	'suse_linux_10_0' => Array(
 		'label' => 'SUSE Linux 10.0',
 		'services' => Array(
