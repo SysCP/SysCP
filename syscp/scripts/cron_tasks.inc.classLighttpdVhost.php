bd4fe43def2147d108db6dec163038265d583839
Florian Aders <EleRas@syscp.org>
2008-06-17 15:58:49 +0000

    Begin of specialized webserver-classes
    git-svn-id: file:///var/svn/trunk@2074 45fdb5c4-e40b-0410-b369-9aab4fe9a275

---
+++ syscp/scripts/cron_tasks.inc.classLighttpdVhost.php
@@ -0,0 +1,25 @@
+<?php
+
+class LighttpdVhost extends vHost
+{
+    public function getVhost()
+    {
+        $domains = '';
+        foreach($this->wwwdomains as $domain)
+        {
+            $domains .= "|(^(www\.|){$domain}$)";
+        }
+
+        foreach($this->wildcarddomains as $domain)
+        {
+            $domains .= "|(.*\.{$domain}$)";
+        }
+
+        foreach($this->domains as $domain)
+        {
+            $domains .= "|(^{$domain}$)";
+        }
+        $domains = substr($domains, 1);
+        $vhost = '$HTTP["host"] =~ "^' . $domains . '$" {' . "\n";
+    }
+}
