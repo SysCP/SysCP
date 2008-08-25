6399619d23b434df5eceed197915a188fb130b0d
Florian Aders <EleRas@syscp.org>
2008-08-25 13:34:20 +0000

    Updated french languagefiles, thx to roms for translating
    git-svn-id: file:///var/svn/trunk/syscp@2173 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- install/lng/french.lng.php
+++ install/lng/french.lng.php
@@ -2,7 +2,7 @@
 
 /**
  * This file is part of the SysCP project.
- * Copyright (c) 2003-2007 the SysCP Team (see authors).
+ * Copyright (c) 2003-2008 the SysCP Team (see authors).
  *
  * For the full copyright and license information, please view the COPYING
  * file that was distributed with this source code. You can also view the
@@ -10,6 +10,7 @@
  *
  * @copyright  (c) the authors
  * @author     Tim Zielosko <mail@zielosko.net>
+ * @author     Romain MARIADASSOU <roms2000@free.fr>
  * @license    GPLv2 http://files.syscp.org/misc/COPYING.txt
  * @package    Language
  * @version    $Id$
@@ -19,41 +20,42 @@
  * Begin
  */
 
-$lng['install']['language'] = 'Langue d´Installation';
-$lng['install']['welcome'] = 'Bienvenue à l´installation de SysCP';
-$lng['install']['welcometext'] = 'Merci beacoup d´avoir choisi SysCP. Pour installer SysCP remplissez les cases ci-dessous avec les dates demandées.<br /><b>Attention:</b> Une banque de données déjà existante qui a le même nom que vous choisissez ci-dessous va être effacée!';
-$lng['install']['database'] = 'Banque de données';
-$lng['install']['mysql_hostname'] = 'Hostname MySQL';
-$lng['install']['mysql_database'] = 'Banque de données MySQL';
-$lng['install']['mysql_unpriv_user'] = 'Utilisateur pour l´accès inprivilégié à MySQL';
-$lng['install']['mysql_unpriv_pass'] = 'Mot de passe pour l´accès inprivilégié à MySQL';
-$lng['install']['mysql_root_user'] = 'Utilisateur pour l´accès root à MySQL';
-$lng['install']['mysql_root_pass'] = 'Mot de passe pour l´accès root à MySQL';
-$lng['install']['admin_account'] = 'Accès administrative';
-$lng['install']['admin_user'] = 'Login de l´administrateur';
-$lng['install']['admin_pass'] = 'Mot de passe de l´administrateur';
-$lng['install']['admin_pass_confirm'] = 'Mot de passe de l´administrateur (Confirmation)';
-$lng['install']['serversettings'] = 'Configuration du server';
-$lng['install']['servername'] = 'Nom du server (FQDN)';
-$lng['install']['serverip'] = 'Adresse IP du server';
+$lng['install']['language'] = 'Langue d\'installation';
+$lng['install']['welcome'] = 'Bienvenue à l\'installation de SysCP';
+$lng['install']['welcometext'] = 'Merci beaucoup d\'avoir choisi SysCP. Pour installer SysCP remplissez les cases ci-dessous avec les informations demandées.<br /><b>Attention :</b> Si vous entrez le nom d\'une base de données existante, celle-ci sera effacée !';
+$lng['install']['database'] = 'Base de données';
+$lng['install']['mysql_hostname'] = 'Nom d\'hôte du serveur MySQL';
+$lng['install']['mysql_database'] = 'Base de données MySQL';
+$lng['install']['mysql_unpriv_user'] = 'Utilisateur pour l\'accès non privilégié à MySQL';
+$lng['install']['mysql_unpriv_pass'] = 'Mot de passe pour l\'accès non privilégié à MySQL';
+$lng['install']['mysql_root_user'] = 'Utilisateur pour l\'accès root à MySQL';
+$lng['install']['mysql_root_pass'] = 'Mot de passe pour l\'accès root à MySQL';
+$lng['install']['admin_account'] = 'Accès administratif';
+$lng['install']['admin_user'] = 'Login de l\'administrateur';
+$lng['install']['admin_pass'] = 'Mot de passe de l\'administrateur';
+$lng['install']['admin_pass_confirm'] = 'Mot de passe de l\'administrateur (confirmation)';
+$lng['install']['serversettings'] = 'Configuration du serveur';
+$lng['install']['servername'] = 'Nom du serveur (FQDN)';
+$lng['install']['serverip'] = 'Adresse IP du serveur';
+$lng['install']['apacheversion'] = 'Version du serveur Apache';
 $lng['install']['next'] = 'Continuer';
 
 /**
  * Progress
  */
 
-$lng['install']['testing_mysql'] = 'Verifiant le login root de MySQL...';
-$lng['install']['erasing_old_db'] = 'Effacant la vielle banque de données...';
-$lng['install']['create_mysqluser_and_db'] = 'Créant banque de données et utilisateur...';
-$lng['install']['testing_new_db'] = 'Verifiant banque de données et utilisateur...';
-$lng['install']['importing_data'] = 'Important les données dans la banque de données...';
-$lng['install']['changing_data'] = 'Conformant les données importés...';
-$lng['install']['adding_admin_user'] = 'Appliquant l´administrateur...';
-$lng['install']['creating_configfile'] = 'Créant le fichier de configuration...';
-$lng['install']['creating_configfile_succ'] = 'OK, userdata.inc.php était sauvegardé à lib/.';
-$lng['install']['creating_configfile_temp'] = 'Fichier était sauvegardé à /tmp/userdata.inc.php, s´il-vous plait le déplacer à lib/.';
-$lng['install']['creating_configfile_failed'] = 'Erreur en créant lib/userdata.inc.php, s´il-vous plait créez le avec le content ci-dessous:';
-$lng['install']['syscp_succ_installed'] = 'SysCP était installé avec succès.';
-$lng['install']['click_here_to_login'] = 'Continuer à l´écran login.';
+$lng['install']['testing_mysql'] = 'Vérification du login root de MySQL ...';
+$lng['install']['erasing_old_db'] = 'Effacement de l\'ancienne base de données ...';
+$lng['install']['create_mysqluser_and_db'] = 'Création de la base de données puis des utilisateurs ...';
+$lng['install']['testing_new_db'] = 'Vérification de la base de données et des utilisateurs ...';
+$lng['install']['importing_data'] = 'Importation des informations dans la base de données ...';
+$lng['install']['changing_data'] = 'Modification des données importés ...';
+$lng['install']['adding_admin_user'] = 'Ajout de l\'utilisateur administrateur ...';
+$lng['install']['creating_configfile'] = 'Création du fichier de configuration ...';
+$lng['install']['creating_configfile_succ'] = 'OK, userdata.inc.php a été sauvegardé dans le dossier lib/ de SysCP.';
+$lng['install']['creating_configfile_temp'] = 'Le fichier a été sauvegardé dans /tmp/userdata.inc.php, veuillez le déplacer / copier dans le dossier lib/ de SysCP.';
+$lng['install']['creating_configfile_failed'] = 'Erreur en créant le fichier lib/userdata.inc.php, veuillez le créer avec le contenu ci-dessous :';
+$lng['install']['syscp_succ_installed'] = 'SysCP a été installé correctement.';
+$lng['install']['click_here_to_login'] = 'Cliquez ici pour vous rendre à l\'invite de connexion.';
 
 ?>
