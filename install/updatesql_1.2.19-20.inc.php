fc4e53735b7b2e0337572318ac1b1f85bb3e2d26
Florian Aders <EleRas@syscp.org>
2008-09-01 19:17:23 +0000

    Eh, the files should be in the last commit, sorry!
    git-svn-id: file:///var/svn/trunk/syscp@2189 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- install/updatesql_1.2.19-20.inc.php
+++ install/updatesql_1.2.19-20.inc.php
@@ -728,6 +728,19 @@ else
 		$db->query($query);
 		$settings['panel']['version'] = '1.2.19-svn25';
 	}
+
+	if($settings['panel']['version'] == '1.2.19-svn25')
+	{
+		$updatelog->logAction(ADM_ACTION, LOG_WARNING, "Updating from 1.2.19-svn25 to 1.2.19-svn26");
+		$db->query("INSERT INTO " . TABLE_PANEL_LANGUAGE . " SET `language` = 'Swedish', `file = 'lng/swedish.lng.php';");
+
+		// set new version
+
+		$query = 'UPDATE `%s` SET `value` = \'1.2.19-svn26\' WHERE `settinggroup` = \'panel\' AND `varname` = \'version\'';
+		$query = sprintf($query, TABLE_PANEL_SETTINGS);
+		$db->query($query);
+		$settings['panel']['version'] = '1.2.19-svn26';
+	}
 }
 
 // php filter-extension check
