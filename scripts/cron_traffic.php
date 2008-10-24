d3c077e02ee26b7406d37a8ec95bc87a3e742946
Florian Lippert <flo@syscp.org>
2008-10-24 12:28:12 +0000

    Made webalizer mandatory again. Note: AWstats not included in configfiles, needs fix
    git-svn-id: file:///var/svn/trunk/syscp@2257 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- scripts/cron_traffic.php
+++ scripts/cron_traffic.php
@@ -124,10 +124,7 @@ while($row = $db->fetch_array($result))
 					unset($domainlist[$row['customerid']][$domainid]);
 				}
 
-				if($settings['system']['webalizer_enabled'] == '1')
-				{
-					$httptraffic+= floatval(callWebalizerGetTraffic($row['loginname'] . '-' . $domain, $row['documentroot'] . '/webalizer/' . $domain . '/', $domain, $domainlist[$row['customerid']]));
-				}
+				$httptraffic+= floatval(callWebalizerGetTraffic($row['loginname'] . '-' . $domain, $row['documentroot'] . '/webalizer/' . $domain . '/', $domain, $domainlist[$row['customerid']]));
 			}
 		}
 
@@ -138,10 +135,7 @@ while($row = $db->fetch_array($result))
 			safeSQLLogfile($domainlist[$row['customerid']], $row['loginname']);
 		}
 
-		if($settings['system']['webalizer_enabled'] == '1')
-		{
-			$httptraffic+= floatval(callWebalizerGetTraffic($row['loginname'], $row['documentroot'] . '/webalizer/', $caption, $domainlist[$row['customerid']]));
-		}
+		$httptraffic+= floatval(callWebalizerGetTraffic($row['loginname'], $row['documentroot'] . '/webalizer/', $caption, $domainlist[$row['customerid']]));
 	}
 
 	/**
