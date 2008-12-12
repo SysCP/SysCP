46b71546a86ff54bc94bce66ac0ec8e2f6d9d2ba
Florian Lippert <flo@syscp.org>
2008-12-12 16:06:01 +0000

    Merging branches/radiation/ticket_1088 to trunk/syscp, fixes #1088
    git-svn-id: file:///var/svn/trunk/syscp@2540 45fdb5c4-e40b-0410-b369-9aab4fe9a275

--- admin_settings.php
+++ admin_settings.php
@@ -1132,12 +1132,10 @@ if(($page == 'settings' || $page == 'overview')
 				if((int)$value == 1)
 				{
 					$db->query('UPDATE `' . TABLE_PANEL_NAVIGATION . '` SET `required_resources` = "phpenabled" WHERE `url` = "customer_aps.nourl"');
-					$db->query('UPDATE `' . TABLE_PANEL_NAVIGATION . '` SET `required_resources` = "can_manage_aps_packages" WHERE `url` = "admin_aps.nourl"');
 				}
 				else
 				{
 					$db->query('UPDATE `' . TABLE_PANEL_NAVIGATION . '` SET `required_resources` = "aps.aps_active" WHERE `url` = "customer_aps.nourl"');
-					$db->query('UPDATE `' . TABLE_PANEL_NAVIGATION . '` SET `required_resources` = "aps.aps_active" WHERE `url` = "admin_aps.nourl"');
 				}
 			}
 
